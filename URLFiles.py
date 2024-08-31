976d436ea66d59632b66b3b88ebbd28138ecb0d3
Branden <brandenkenn@gmail.com>
2024-08-30 23:58:22 -0400

    Update and rename URLFiles.py to expressions_quiz.py

--- URLFiles.py
+++
@@ -1,1 +0,0 @@
-# First quiz on expressions: https://py2.codeskulptor.org/#user51_jKsolkHb6C_2.py
---
+++ expressions_quiz.py
@@ -0,0 +1,21 @@
+# First quiz on expressions: https://py2.codeskulptor.org/#user51_jKsolkHb6C_2.py
+
+print "1. " + str(13 * 5280)
+
+print "2. " + str((7 * 60 * 60) + (21 * 60) + 37)
+
+print "3. " + str((2 * 4) + (2 * 7))
+
+print "4. " + str(4 * 7)
+
+print "5. " + str(8 * 2 * 3.14)
+
+print "6. " + str(3.14 * 8 ** 2)
+
+print "7. " + str(1000 * (1 + 0.01 * 7) ** 10)
+
+print "8. " + "My name is " + "Joe " + "Warren."
+
+print "9. Joe Warren is " + str(52) + " years old."
+
+print "10. " + str(((5 - 2) ** 2 + ( 6 - 2) ** 2) ** 0.5)
